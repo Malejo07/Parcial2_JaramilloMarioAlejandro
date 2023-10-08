f266094d946e5fadae1bb0a509e4ff413d30540b
Mario Alejandro Jaramillo <malejo9002@gmail.com>
2023-10-08 05:50:41 -0500

    The fields of the Natural Person table are created

--- PersonData/DAL/Entities/Entity.cs
+++ PersonData/DAL/Entities/Entity.cs
@@ -1,12 +1,16 @@
-﻿namespace PersonData.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonData.DAL.Entities
 {
     public class Entity
     {
-        
+        [Required]
         public Guid Id { get; set; }
 
+        [Display(Name = "Fecha de Creación")]
         public DateTime? CreatedDate { get; set; }
 
+        [Display(Name = "Fecha de Modificación")]
         public DateTime? ModifiedDate { get; set; }
     }
 }
--- PersonData/DAL/Entities/NaturalPerson.cs
+++ PersonData/DAL/Entities/NaturalPerson.cs
@@ -1,7 +1,27 @@
-﻿namespace PersonData.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonData.DAL.Entities
 {
     public class NaturalPerson : Entity
     {
+        [Display(Name = "Nombre Completo")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "¡El campo {0} es requerido!")]
+        public string FullName { get; set; }
 
+        [Display(Name = "Email")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "¡El campo {0} es requerido!")]
+        public string Email { get; set; }
+
+        [Display(Name = "Año de nacimiento")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "¡El campo {0} es requerido!")]
+        public string BirthYear { get; set; }
+
+        [Display(Name = "Edad")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")] 
+        [Required(ErrorMessage = "¡El campo {0} es requerido!")]
+        public string Age { get; set; }
     }
 }
