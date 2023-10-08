1b4fa7ada1cf33855fa8db8385c02995026d5df1
Mario Alejandro Jaramillo <malejo9002@gmail.com>
2023-10-08 07:57:32 -0500

    Bd configured ans created with EF Core

---
+++ PersonData/DAL/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonData.DAL.Entities;
+using System.Diagnostics.Metrics;
+
+namespace PersonData.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<NaturalPerson>().HasIndex(c => c.Email).IsUnique();
+        }
+
+        public DbSet<NaturalPerson> NaturalPersons { get; set; }
+    }
+}
---
+++ PersonData/Migrations/20231008124602_InitialBDTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonData.Migrations
+{
+    public partial class InitialBDTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NaturalPersons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    BirthYear = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NaturalPersons", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NaturalPersons_Email",
+                table: "NaturalPersons",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NaturalPersons");
+        }
+    }
+}
